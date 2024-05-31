f8e765cc792a646ce98eb0c005f3069c1f3d8bbd
David Fritcher <dfritcher@gmail.com>
2024-05-31 11:54:44 -0700

    Update main.js
    Debugging

--- js/main.js
+++ js/main.js
@@ -254,7 +254,7 @@
 		      error: function() {
 
 		      	sLoader.fadeOut(); 
-		      	$('#message-warning').html("Something went wrong. Please try again.");
+		      	$('#message-warning').html("Something went wrong. Please try emailing from your provider.");
 		         $('#message-warning').fadeIn();
 
 		      }
